51aa49962ba92762ebab3b67e2bee4d1445778c8
Gergely Imreh <imrehg@gmail.com>
2011-02-25 01:11:41 +0800

    going along the path: toggle button, saved preferences,

--- src/com/startupbus/location/Logger.java
+++ src/com/startupbus/location/Logger.java
@@ -9,15 +9,20 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ToggleButton;
+import android.widget.Toast;
 
 import com.startupbus.location.service.GPSLoggerService;
 
 import com.simplegeo.client.SimpleGeoPlacesClient;
+import com.simplegeo.client.SimpleGeoStorageClient;
 import com.simplegeo.client.callbacks.FeatureCollectionCallback;
 import com.simplegeo.client.types.Feature;
 import com.simplegeo.client.types.FeatureCollection;
 import com.simplegeo.client.types.Point;
 import com.simplegeo.client.types.Geometry;
+import com.simplegeo.client.types.Record;
 
 // import com.simplegeo.android.cache.CommitLog;
 import com.simplegeo.client.handler.GeoJSONHandler;
@@ -39,26 +44,60 @@ import org.json.JSONException;
 
 import java.util.*;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import android.content.SharedPreferences;
+import android.text.TextWatcher;
+
 
 public class Logger extends Activity implements OnClickListener {
+    public static final String PREFS_NAME = "BusdroidPrefs";
+
     private static final String TAG = "Logger";
     private LocationManager myManager;
     FeatureCollection collection;
     Button buttonStart, buttonStop;
     TextView debugArea;
+    EditText sglayeredit;
+    String buslayer;
+    ToggleButton gpstoggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
-	
+
+	// Restore preferences
+	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	String buslayer = settings.getString("BusLayer", "");
+
 	buttonStart = (Button) findViewById(R.id.buttonStart);
 	buttonStop = (Button) findViewById(R.id.buttonStop);
 
-	debugArea = (TextView) findViewById(R.id.debug);
+	debugArea = (TextView) findViewById(R.id.debugArea);
+
+	sglayeredit = (EditText) findViewById(R.id.sglayeredit);
+	sglayeredit.setText(buslayer);
 
 	buttonStart.setOnClickListener(this);
 	buttonStop.setOnClickListener(this);
+
+	// Todo: set start state depending on whether service is running
+	gpstoggle = (ToggleButton) findViewById(R.id.gpsToggle);
+	gpstoggle.setOnClickListener(new OnClickListener() {
+		public void onClick(View v) {
+		    // Perform action on clicks
+		    if (gpstoggle.isChecked()) {
+			saveSettings();
+			startGPS();
+			Toast.makeText(Logger.this, "Checked", Toast.LENGTH_SHORT).show();
+		    } else {
+			stopGPS();
+			Toast.makeText(Logger.this, "Not checked", Toast.LENGTH_SHORT).show();
+		    }
+		}
+	    });
 
 	// LocationManager locator = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 	myManager = (LocationManager) getSystemService(LOCATION_SERVICE); 
@@ -75,62 +114,101 @@ public class Logger extends Activity implements OnClickListener {
 	// }
 	// // locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE); Location location = locationManager.getCurrentLocation("gps");
 
-	SimpleGeoPlacesClient client = SimpleGeoPlacesClient.getInstance();
+	SimpleGeoStorageClient client = SimpleGeoStorageClient.getInstance();
 	client.getHttpClient().setToken("CrQ8RDznnjEhwUCGn5Uv9G3h9kR4xcLK", "MtLzaKmMP8C2DfYBDWUemZ6pRLZQe2cT");
-	// Point loc = new Point(l.getLongitude(), l.getLatitude());
-	Point loc = new Point(l.getLatitude(), l.getLongitude());
-	debugArea.setText(String.format("Last location:\n%.7f, %.7f\n(from %s)",
-					loc.getLat(),
-					loc.getLon(),
-					l.getProvider()
-					)
-			  );
-	// try {
-	//     collection = client.search(37.7787, -122.3896, "",  "", 25.0);
-	// } catch (IOException e) {
-	//     debugArea.setText(e.getMessage());
-	// }
-	String text = "";
-	// try {
-	//     text = collection.toJSONString();
-	// } catch(JSONException e) {
-	//     debugArea.setText(e.getMessage());
-	// }
 
+	Point loc = new Point(l.getLatitude(), l.getLongitude());
+	// debugArea.setText(String.format("Last location:\n%.7f, %.7f\n(from %s)",
+	// 				loc.getLat(),
+	// 				loc.getLon(),
+	// 				l.getProvider()
+	// 				)
+	// 		  );
+	// // try {
+	// //     collection = client.search(37.7787, -122.3896, "",  "", 25.0);
+	// // } catch (IOException e) {
+	// //     debugArea.setText(e.getMessage());
+	// // }
+	// String text = "";
+	// // try {
+	// //     text = collection.toJSONString();
+	// // } catch(JSONException e) {
+	// //     debugArea.setText(e.getMessage());
+	// // }
+
+
+	String recordId = String.format("bus_%d", 1234);
+	String layer = "com.startupbus.test";
+	String rectype= "Feature";
+	Record update = new Record();
 
 	HashMap hm = new HashMap();
-	hm.put("postcode", "10617");
-	Feature newplace = new Feature();
-	newplace.setType("Feature");
-	newplace.setGeometry(new Geometry(loc));
-	newplace.setType("StartupBusTest01");
-	newplace.setProperties(hm);
-	newplace.setSimpleGeoId("ABC");
+	hm.put("testing", true);
+	Record statusupdate = new Record(recordId, layer, rectype, loc.getLon(), loc.getLat());
+
+	// newplace.setGeometry(new Geometry(loc));
+	// newplace.setType("StartupBusTest01");
+	// newplace.setProperties(hm);
+	// newplace.setSimpleGeoId("ABC");
+	String text = "";
 	try {
-	    text = newplace.toJSONString();
+	    text = statusupdate.toJSONString();
 	} catch(JSONException e) {
 	    debugArea.setText(e.getMessage());
 	}
 	debugArea.setText(text);
 
 	
-	HashMap ret = new HashMap();
-	try {
-	    ret = client.addPlace(newplace);
-	} catch(IOException e) {
-	    debugArea.setText("IO>"+e.getMessage()+"\n");
-	} catch(JSONException e) {
-	    debugArea.setText("JSON>"+e.getMessage()+"\n");
-	}
+	// // HashMap ret = new HashMap();
+	// try {
+	//     client.addOrUpdateRecord(statusupdate);
+	// } catch(IOException e) {
+	//     debugArea.setText("IO>"+e.getMessage()+"\n");
+	// } catch(JSONException e) {
+	//     debugArea.setText("JSON>"+e.getMessage()+"\n");
+	// }
 
-	Set set = ret.entrySet();
-	Iterator i = set.iterator();
-	while(i.hasNext()){
-	    Map.Entry me = (Map.Entry)i.next();
-	    debugArea.append(">"+me.getKey() + "< : " + me.getValue() );
-	}
+	// // Set set = ret.entrySet();
+	// // Iterator i = set.iterator();
+	// // while(i.hasNext()){
+	// //     Map.Entry me = (Map.Entry)i.next();
+	// //     debugArea.append(">"+me.getKey() + "< : " + me.getValue() );
+	// // }
 	
     }
+
+   // class MyData {
+   //      public MyData( String spinnerText, String value ) {
+   //          this.spinnerText = spinnerText;
+   //          this.value = value;
+   //      }
+
+   //      public String getSpinnerText() {
+   //          return spinnerText;
+   //      }
+
+   //      public String getValue() {
+   //          return value;
+   //      }
+
+   //      public String toString() {
+   //          return spinnerText;
+   //      }
+
+   //      String spinnerText;
+   //      String value;
+   //  }
+
+    public void startGPS() {
+	startService(new Intent(Logger.this,
+				GPSLoggerService.class));
+    }
+
+    public void stopGPS() {
+	stopService(new Intent(Logger.this,
+				GPSLoggerService.class));
+    }
+
 
     public void onClick(View src) {
 	switch (src.getId()) {
@@ -146,6 +224,30 @@ public class Logger extends Activity implements OnClickListener {
 				   GPSLoggerService.class));
    	    debugArea.setText("Noeh");
 	    break;
+	case R.id.gpsToggle:
+	    debugArea.setText(sglayeredit.getText().toString());
+	    break;
 	}
+
     }
+
+    public void saveSettings(){
+	// Save Shared Preferences
+
+	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	SharedPreferences.Editor editor = settings.edit();
+
+	// Name of bus' layer on SimpleGeo
+	buslayer = sglayeredit.getText().toString();
+	editor.putString("BusLayer", buslayer);
+	
+	// Commit the edits!
+	editor.commit();	
+    }
+
+    protected void onStop(){
+	super.onStop();
+	saveSettings();
+    }
+
 }
