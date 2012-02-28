4ae8d6190a7c0b237bb2bd7ec3ff291298153213
Gergely Imreh <imrehg@gmail.com>
2012-02-28 14:47:46 +0800

    remote pull: working but dirty version

--- src/com/startupbus/location/BusDroid.java
+++ src/com/startupbus/location/BusDroid.java
@@ -52,6 +52,25 @@ import android.view.MenuItem;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.BufferedReader;
+
+import org.json.JSONTokener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.widget.Toast;
+import android.app.ProgressDialog;
 
 public class BusDroid extends Activity implements OnClickListener {
     public static final String PREFS_NAME = "BusdroidPrefs";
@@ -68,6 +87,7 @@ public class BusDroid extends Activity implements OnClickListener {
     private int bus_id;
     private int refresh_interval;
     private String remote_server;
+    private String remote_config;
     private TextView push_location_view;
 
     final String APP_ID = "3bc0af918733f74f08d0b274e7ede7b0";
@@ -79,6 +99,17 @@ public class BusDroid extends Activity implements OnClickListener {
     private SharedPreferences settings;
     private SharedPreferences.Editor settingsEditor;
 
+    private static final int MCONFIG = 5000;
+    private static final int MBUSES = 5001;
+    private static final int MINTERVAL = 5002;
+    private static final int MENDPOINT = 5003;
+
+    private static final int GBUSES = 1;
+
+    private JSONArray buses;
+    private String busesJSON;
+
+    /* Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -90,6 +121,9 @@ public class BusDroid extends Activity implements OnClickListener {
 
 	bus_id = settings.getInt("bus_id", 7);
 	refresh_interval = settings.getInt("refresh_interval", 5);
+	remote_server = settings.getString("remote_server", "");
+	remote_config = settings.getString("remote_config", "");
+	busesJSON = settings.getString("busesJSON", "[]");
 
 	buttonStart = (Button) findViewById(R.id.buttonStart);
 	buttonStop = (Button) findViewById(R.id.buttonStop);
@@ -101,31 +135,74 @@ public class BusDroid extends Activity implements OnClickListener {
 	buttonStart.setOnClickListener(this);
 	buttonStop.setOnClickListener(this);
 
-	// Set city mapping - quick and dirty hard-coding
-	city_map = new HashMap(7);
-	city_map.put("New York", new Integer(1));
-	city_map.put("San Francisco", new Integer(2));
-	city_map.put("Silicon Valley", new Integer(3));
-	city_map.put("Chicago", new Integer(4));
-	city_map.put("Miami", new Integer(5));
-	city_map.put("Cleveland", new Integer(6));
-	city_map.put("Europe", new Integer(7));
-
-	city_map_reverse = new HashMap(7);
-	Set s= city_map.keySet();
-	Iterator i=s.iterator();
-	String city;
-	while(i.hasNext()) {
-	    city = (String) i.next();
-	    city_map_reverse.put(city_map.get(city), city);
-	}
-	cityview.setText(city_map_reverse.get(bus_id));
-
-	remote_server = settings.getString("remote_server", "http://startupbus.com/api/locations");
-	settingsEditor.putString("remote_server", remote_server);
-	settingsEditor.commit();
-
 	push_location_view.setText(remote_server);
+	cityview.setText(getBusName(getBusIndex(bus_id)));
+
+    }
+
+    public void makeToast(String text) {
+	Toast.makeText(BusDroid.this, text, Toast.LENGTH_LONG).show();
+    }
+
+    public String getBusName(int index) {
+	JSONArray buses;
+	JSONObject bus;
+	String busname;
+	try {
+	    buses = new JSONArray(busesJSON);
+	    bus = buses.optJSONObject(index);
+	    busname = bus.getString("name");
+	} catch(Exception e) {
+	    busname = "?";
+	}
+	return busname;
+    }
+
+    public int getBusIndex(int id) {
+	JSONArray buses;
+	JSONObject bus;
+	String busname = "";
+	int index = -1;
+	try {
+	    buses = new JSONArray(busesJSON);
+	    for (int i = 0; i < buses.length(); i++) {
+		bus = buses.optJSONObject(i);
+		if (id == bus.getInt("id")) {
+		    index = i;
+		}
+	    }
+	} catch(JSONException e) {
+
+	} finally {
+	    return index;
+	}
+    }
+
+    public static String getStringContent(String uri) throws Exception {
+	try {
+	    HttpClient client = new DefaultHttpClient();
+	    HttpGet request = new HttpGet();
+	    request.setURI(new URI(uri));
+	    HttpResponse response = client.execute(request);
+	    InputStream ips  = response.getEntity().getContent();
+	    BufferedReader buf = new BufferedReader(new InputStreamReader(ips,"UTF-8"));
+
+	    StringBuilder sb = new StringBuilder();
+	    String s;
+	    while(true )
+		{
+		    s = buf.readLine();
+		    if(s==null || s.length()==0)
+			break;
+		    sb.append(s);
+		}
+	    buf.close();
+	    ips.close();
+	    return sb.toString();
+        }
+	finally {
+	    // any cleanup code...
+	}
     }
 
     public void testData() {
@@ -227,8 +304,13 @@ public class BusDroid extends Activity implements OnClickListener {
 	// Remote server to push to
 	settingsEditor.putString("remote_server", remote_server);
 
+	settingsEditor.putString("remote_config", remote_config);
+
+	settingsEditor.putString("busesJSON", busesJSON);
+
 	// Commit the edits!
 	settingsEditor.commit();
+	push_location_view.setText(remote_server);
     }
 
     protected void onStop(){
@@ -239,34 +321,34 @@ public class BusDroid extends Activity implements OnClickListener {
     // Config menu items
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+	// Create all the menus
+
+	// Remote configuration pull
+	SubMenu confpullMenu = menu.addSubMenu(0, MCONFIG, 0, "Get configuration");
+	confpullMenu.setIcon(android.R.drawable.ic_menu_manage);
+
+	// Choose a bus
+	SubMenu busMenu = menu.addSubMenu(GBUSES, MBUSES, 0, "Choose Bus");
+	busMenu.setIcon(android.R.drawable.ic_menu_mylocation);
+	// try {
+	//     JSONArray buslist = new JSONArray(busesJSON);
+	//     for (int x = 0; x < buslist.length(); x++ ) {
+	// 	try {
+	// 	    JSONObject bus = buslist.getJSONObject(x);
+	// 	    String name = bus.getString("name");
+	// 	    busMenu.add(GBUSES, x, x, name);
+	// 	} catch(JSONException e) {
+	// 	    Log.i("Error", "Bus list data broken");
+	// 	}
+	//     }
+	// } catch(Exception e) {
+	//     Log.i("Error", "Bus list broken");
+	// }
+
+	// Rest of the stuff....
 	MenuInflater inflater = getMenuInflater();
 	inflater.inflate(R.menu.menu, menu);
-
-	// Load defaults and use them
-	switch(bus_id) {
-	case 1:
-	    setThisChecked(menu, R.id.bus_newyork_check);
-	    break;
-	case 2:
-	    setThisChecked(menu, R.id.bus_sanfrancisco_check);
-	    break;
-	case 3:
-	    setThisChecked(menu, R.id.bus_siliconvalley_check);
-	    break;
-	case 4:
-	    setThisChecked(menu, R.id.bus_chicago_check);
-	    break;
-	case 5:
-	    setThisChecked(menu, R.id.bus_miami_check);
-	    break;
-	case 6:
-	    setThisChecked(menu, R.id.bus_cleveland_check);
-	    break;
-	case 7:
-	default :
-	    setThisChecked(menu, R.id.bus_europe_check);
-	    break;
-	}
 
 	switch(refresh_interval) {
 	case 10:
@@ -284,6 +366,30 @@ public class BusDroid extends Activity implements OnClickListener {
 	    break;
 	}
 
+	return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu) {
+	MenuItem busMenuItem = menu.findItem(MBUSES);
+	if (busMenuItem != null) {
+	    SubMenu busMenu = busMenuItem.getSubMenu();
+	    busMenu.clear();
+	    try {
+		JSONArray buslist = new JSONArray(busesJSON);
+		for (int x = 0; x < buslist.length(); x++ ) {
+		    try {
+			JSONObject bus = buslist.getJSONObject(x);
+			String name = bus.getString("name");
+			busMenu.add(GBUSES, x, x, name);
+		} catch(Exception e) {
+			Log.i("Error", "Bus list data broken:"+e.toString());
+		    }
+		}
+	    } catch(Exception e) {
+		Log.i("Error", "Bus list broken");
+	    }
+	}
 	return true;
     }
 
@@ -305,13 +411,67 @@ public class BusDroid extends Activity implements OnClickListener {
 	restartGPS();
     }
 
-    public void setBusID(int new_bus_id) {
-	// Update bus id in database
-	Log.i(tag, String.format("New Bus ID set: %d (was %d)", new_bus_id, bus_id));
-	bus_id = new_bus_id;
-	cityview.setText(city_map_reverse.get(bus_id));
+    public void setBusID(int index) {
+	JSONArray buses;
+	JSONObject bus;
+	int busid = 0;
+	String busname = "";
+	try {
+	    buses = new JSONArray(busesJSON);
+	    bus = buses.optJSONObject(index);
+	    busid = bus.getInt("id");
+	    busname = bus.getString("name");
+	} catch(JSONException e) {
+	    //
+	}
+	bus_id = busid;
+	cityview.setText(busname);
 	saveSettings(true);
 	restartGPS();
+    }
+
+
+    public void get_remote_config_dialog() {
+	AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+	alert.setTitle("Remote config URL");
+	alert.setMessage("Pull new configuration from remote server");
+
+	// Set an EditText view to get user input 
+        final EditText input = new EditText(this);
+	alert.setView(input);
+
+	input.setText(remote_config);
+
+	alert.setPositiveButton("Get config", new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int whichButton) {
+		    remote_config = input.getText().toString();
+		    try {
+			String remotedata = getStringContent(remote_config);
+			Log.i("DATA", remotedata);
+			JSONObject object = (JSONObject) new JSONTokener(remotedata).nextValue();
+			// Save settings
+			buses = object.getJSONArray("buses");
+			busesJSON = object.getJSONArray("buses").toString();
+			remote_server = object.getString("endpoint");
+			saveSettings(true);
+			restartGPS();
+			makeToast("Configuration update successful.");
+		    } catch (Exception e) {
+			makeToast("Something went wrong, please check your network connection and the URL you gave.");
+			Log.i("Remote config error:", e.toString());
+		    } finally {
+		    }
+		}
+	    });
+
+	alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int whichButton) {
+		    // Canceled.
+		}
+	    });
+
+	alert.show();
     }
 
     public void set_location_dialog() {
@@ -346,6 +506,34 @@ public class BusDroid extends Activity implements OnClickListener {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+	int gID = item.getGroupId();
+	int iID = item.getItemId();
+
+	if (gID == GBUSES) {
+	    switch(iID) {
+	    case MBUSES:
+		return true;
+
+	    default:
+		JSONArray buses;
+		JSONObject bus;
+		int busid;
+		String busname;
+
+		try {
+		    buses = new JSONArray(busesJSON);
+		    bus = buses.optJSONObject(iID);
+		    busid = bus.getInt("id");
+		    busname = bus.getString("name");
+		} catch(JSONException e) {
+		    return true;
+		}
+		setBusID(iID);
+		return true;
+	    }
+	}
+
+
 	// Handle item selection
 	switch (item.getItemId()) {
 	case R.id.bus_id_setting:
@@ -383,6 +571,9 @@ public class BusDroid extends Activity implements OnClickListener {
 
 	case R.id.push_location:
 	    set_location_dialog();
+	    return true;
+	case MCONFIG:
+	    get_remote_config_dialog();
 	    return true;
 
 	default:
